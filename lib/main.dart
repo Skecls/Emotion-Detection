0cc0acf4aaaf9866c6acc8fbd39d0f14824b75e6
skecls <aman.singh.personalacc@gmail.com>
2023-04-29 14:39:11 +0530

    i dont know what i am doing

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:test/homescreen.dart';
 import 'splash.dart';
 import 'loginpage.dart';
 import 'register.dart';
+import 'profile.dart';
+import 'navbar.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: MyLogin(),
+    home: Splash(),
   ));
 }
